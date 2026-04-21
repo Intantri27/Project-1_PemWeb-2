df9626456508487f008783d4384f87cb74da361b
Intantri27 <trihartatiintan@gmail.com>
2026-04-21 14:59:29 +0700

    add regis form

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App (){
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-4 text-center"> Registrasi Event </h2>
+        <h2 className="text-2xl font-bold mb-4 text-center">Registrasi Event</h2>
         <RegisterForm />
       </div>
     </div>
